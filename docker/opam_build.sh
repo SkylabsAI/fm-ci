b9a53490fe9c9641822e6d40e07d7ebb9fc23704
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2025-10-24 12:57:15 +0200

    opam-release build
    skylabs-fm -> workspace
    skylabs-fm.tar -> workspace.tar

--- docker/opam_build.sh
+++ docker/opam_build.sh
@@ -1,17 +1,18 @@
 #!/bin/bash -evx
 
+ROCQ_LOG_PREFIX="$1"
+
 opam option depext=false
 opam update -y
-opam repo add archive git+https://github.com/ocaml/opam-repository-archive
 
 opam pin add -y -k rsync --recursive -n --with-version dev .
 
-opam install -y $(opam pin | grep -E '/fmdeps/(auto|BRiCk|vendored/(vscoq|coq-lsp))' |
-  grep -E -v 'rocq-bluerock-cpp-(demo|stdlib)' |
-  awk '{print $1}')
+OPAM_PACKAGES=$(eval $2 | awk '{print $1}')
+
+opam install -y ${OPAM_PACKAGES}
 /tmp/files/opam-clean
 
-find $(opam var prefix) \( -path '*bluerock*/*.v' -o -path '*bluerock*/*.ml' \) -print0 |
+find $(opam var prefix) \( -path "*${ROCQ_LOG_PREFIX}*/*.v" -o -path "*${ROCQ_LOG_PREFIX}*/*.ml" \) -print0 |
   xargs -0 truncate --size 0
 
 sudo rm -rf /tmp/files
