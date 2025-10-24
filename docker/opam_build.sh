6cf65bb36c3819a6ddfe94ef41d5f23a851309ef
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2025-10-24 23:10:18 +0200

    Pass OPAM_PACKAGES

--- docker/opam_build.sh
+++ docker/opam_build.sh
@@ -11,8 +11,6 @@ opam pin add -y -k rsync --recursive -n --with-version dev .
 opam install -y ${OPAM_PACKAGES}
 /tmp/files/opam-clean
 
-# $(opam pin | grep -E '/fmdeps/(auto|BRiCk|vendored/(vscoq|coq-lsp))' | grep -E -v 'rocq-bluerock-cpp-(demo|stdlib)' | awk '{print $1}')
-
 find $(opam var prefix) \( -path "*${ROCQ_LOG_PREFIX}*/*.v" -o -path "*${ROCQ_LOG_PREFIX}*/*.ml" \) -print0 |
   xargs -0 truncate --size 0
 
