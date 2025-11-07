aa2910639916cb10fca03f68b5c2709520714d82
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2025-11-07 06:25:24 +0100

    refactor, Makefile: extract docker-img-llvm-basics.mk

---
+++ docker/docker-img-llvm-basics.mk
@@ -0,0 +1,16 @@
+define image-target
+.PHONY: fm-$(BR_FMDEPS_VERSION)-llvm-$1
+fm-$(BR_FMDEPS_VERSION)-llvm-$1: fm-$(BR_FMDEPS_VERSION)-base Dockerfile-llvm
+	@echo "[DOCKER] Building $$@"
+	$(Q)docker buildx build \
+		--platform linux/amd64 \
+		-t $(DOCKER_REPO):$$@ \
+		--build-arg LLVM_MAJ_VER=$1 \
+		--build-arg BASE_IMAGE=$(DOCKER_REPO):$$< \
+		--build-arg \
+		  DOCKER_IMAGE_VERSION="fmdeps.${BR_FMDEPS_VERSION},llvm.${LLVM_VER},image.${BR_IMAGE_VERSION}" \
+		-f Dockerfile-llvm .
+
+DOCKER_BUILD_TARGETS += fm-$(BR_FMDEPS_VERSION)-llvm-$1
+DOCKER_PUSH_TARGETS += push-fm-$(BR_FMDEPS_VERSION)-llvm-$1
+endef
--- docker/docker-img-stage0-llvm.mk
+++ docker/docker-img-stage0-llvm.mk
@@ -1,20 +1,3 @@
-define image-target
-.PHONY: fm-$(BR_FMDEPS_VERSION)-llvm-$1
-fm-$(BR_FMDEPS_VERSION)-llvm-$1: fm-$(BR_FMDEPS_VERSION)-base Dockerfile-llvm
-	@echo "[DOCKER] Building $$@"
-	$(Q)docker buildx build \
-		--platform linux/amd64 \
-		-t $(DOCKER_REPO):$$@ \
-		--build-arg LLVM_MAJ_VER=$1 \
-		--build-arg BASE_IMAGE=$(DOCKER_REPO):$$< \
-		--build-arg \
-		  DOCKER_IMAGE_VERSION="fmdeps.${BR_FMDEPS_VERSION},llvm.${LLVM_VER},image.${BR_IMAGE_VERSION}" \
-		-f Dockerfile-llvm .
-
-DOCKER_BUILD_TARGETS += fm-$(BR_FMDEPS_VERSION)-llvm-$1
-DOCKER_PUSH_TARGETS += push-fm-$(BR_FMDEPS_VERSION)-llvm-$1
-endef
-
 $(foreach llvm,$(LLVM_VERSIONS),\
 	$(eval $(call image-target,$(llvm))))
 
