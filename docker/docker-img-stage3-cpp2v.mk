c0e0478818dd8d9e0086a1d4e63cb8060302022d
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2025-11-07 06:43:56 +0100

    feat,docker: build fm-DATE-stage3 image

---
+++ docker/docker-img-stage3-cpp2v.mk
@@ -0,0 +1,8 @@
+.PHONY: fm-$(BR_FMDEPS_VERSION)-stage3-release
+fm-$(BR_FMDEPS_VERSION)-stage3-release: fm-$(BR_FMDEPS_VERSION)-stage2-llvm-$(LLVM_MAIN_VERSION) prepare-fm-release
+	$(call opam-img-target,\
+	grep -E "(rocq-bluerock-cpp2v|rocq-bluerock-cpp-stdlib|rocq-brick-libstdcpp)")
+# TODO: add rocq-bluerock-scaffold
+
+DOCKER_BUILD_TARGETS += fm-$(BR_FMDEPS_VERSION)-stage3-release
+DOCKER_PUSH_TARGETS += push-fm-$(BR_FMDEPS_VERSION)-stage3-release
