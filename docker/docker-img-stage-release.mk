d7981d86a07ca2a35f083e9827f7b45a073ce966
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2025-11-07 03:22:05 +0100

    Refactor docker-img-stage-release

--- docker/docker-img-stage-release.mk
+++ docker/docker-img-stage-release.mk
@@ -1,31 +1,6 @@
-monorepo_parent=../../../..
-docker_build_folder=docker-opam-release
-
-# XXX missing dependencies!
-# Move this step into a control script instead!
-GEN_FILES += $(monorepo_parent)/$(docker_build_folder)/files/workspace.tar
-$(monorepo_parent)/$(docker_build_folder)/files/workspace.tar:
-	cd $(monorepo_parent); mkdir -p $(docker_build_folder)/files; \
-	  time ./workspace/fmdeps/fm-ci/docker/stable-tar.sh workspace $(docker_build_folder)/files/
-
-CP = $(shell which gcp || which cp)
-.PHONY: prepare-fm-release
-prepare-fm-release: $(monorepo_parent)/$(docker_build_folder)/files/workspace.tar
-	$(CP) -t $(monorepo_parent)/$(docker_build_folder)/files files/dune-config files/LICENSE files/opam-clean
-	$(CP) -t $(monorepo_parent)/$(docker_build_folder) Dockerfile-opam-release
-
 .PHONY: fm-$(BR_FMDEPS_VERSION)-release
-
 fm-$(BR_FMDEPS_VERSION)-release: fm-$(BR_FMDEPS_VERSION)-llvm-$(LLVM_MAIN_VERSION) prepare-fm-release
-	cd $(monorepo_parent)/$(docker_build_folder); \
-	docker buildx build \
-		-f Dockerfile-opam-release \
-		--build-arg BASE_IMAGE=$(DOCKER_REPO):$< \
-		--build-arg ROCQ_LOG_PREFIX=bluerock \
-		--build-arg \
-		OPAM_PACKAGES='opam pin | grep -E "/fmdeps/(auto|BRiCk|vendored/(vscoq|coq-lsp))" | grep -E -v "rocq-bluerock-cpp-(demo|stdlib)"' \
-		-t $(DOCKER_REPO):$@ \
-		.
+	$(call opam-img-target,grep -E "/fmdeps/(auto|BRiCk|vendored/(vscoq|coq-lsp))" | grep -E -v "rocq-bluerock-cpp-(demo|stdlib)")
 
 DOCKER_BUILD_TARGETS += fm-$(BR_FMDEPS_VERSION)-release
 DOCKER_PUSH_TARGETS += push-fm-$(BR_FMDEPS_VERSION)-release
